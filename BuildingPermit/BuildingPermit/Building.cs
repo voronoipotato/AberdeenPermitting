5a52a825aa6d80f480458e9c04961fe175d77ac8
John Reasor <John Reasor@JohnReasor-LT.(none)>
2012-03-13 20:38:27 -0400

    Added Building Class File

---
+++ BuildingPermit/BuildingPermit/Building.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BuildingPermit
+{
+    class Building
+    {
+
+       
+
+        public String BuildingType
+        {
+            get { return BuildingType; }
+            set { BuildingType = value; }
+        }
+       
+        public int EstimatedCost
+        {
+            get { return EstimatedCost; }
+            set { EstimatedCost = value; }
+        }
+        public int totalSquareFeet { get; set; }
+    }
+}
